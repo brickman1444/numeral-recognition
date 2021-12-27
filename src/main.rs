583b14cbad9533e11cc4a341bc8dcf9c8472157c
Zac Gross <brickman1444@gmail.com>
2021-12-26 18:14:40 -0600

    Set up first pass at loading data from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut f = std::fs::File::open("data/t10k-labels.idx1-ubyte").unwrap();
+    let mut buffer = [0; 8];
+
+    let num_bytes_read = f.read(&mut buffer).unwrap();
+    assert!(num_bytes_read == 8)
 }
