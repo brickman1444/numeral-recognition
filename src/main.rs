034011c9e2c4f1ae39782b2552b174544ebea562
Zac Gross <brickman1444@gmail.com>
2021-12-26 20:16:13 -0600

    Read some integers from the labels file

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,16 @@ use std::io::prelude::*;
 
 fn main() {
     let mut f = std::fs::File::open("data/t10k-labels.idx1-ubyte").unwrap();
-    let mut buffer = [0; 8];
 
-    let num_bytes_read = f.read(&mut buffer).unwrap();
-    assert!(num_bytes_read == 8)
+    let mut magic_number_buffer = [0; 4];
+    let num_bytes_read = f.read(&mut magic_number_buffer).unwrap();
+    assert!(num_bytes_read == 4);
+    let labels_magic_number = u32::from_be_bytes(magic_number_buffer);
+    assert!(labels_magic_number == 2049);
+
+    let mut number_of_items_buffer = [0; 4];
+    let num_bytes_read = f.read(&mut number_of_items_buffer).unwrap();
+    assert!(num_bytes_read == 4);
+    let number_of_items = u32::from_be_bytes(number_of_items_buffer);
+    assert!(number_of_items == 10000);
 }
