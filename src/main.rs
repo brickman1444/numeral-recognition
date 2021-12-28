36402f9404fc4728849fa2cbca045826b92beebe
Zac Gross <brickman1444@gmail.com>
2021-12-28 17:09:05 -0600

    Format data for training
    I was able to start training at this point but it ran for a while with no logging of what was happening

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,70 @@
 use std::io::prelude::*;
 
 fn main() {
-    open_labels("data/train-labels.idx1-ubyte");
-    open_labels("data/t10k-labels.idx1-ubyte");
+    std::println!("Open files");
+    let labels = open_labels("data/train-labels.idx1-ubyte");
+    //open_labels("data/t10k-labels.idx1-ubyte");
 
-    open_images("data/train-images.idx3-ubyte");
-    open_images("data/t10k-images.idx3-ubyte");
+    let image_bytes = open_images("data/train-images.idx3-ubyte");
+    //open_images("data/t10k-images.idx3-ubyte");
+
+    let pixels_per_image = 28 * 28;
+
+    std::println!("Construct training data");
+    let mut examples : std::vec::Vec<(std::vec::Vec<f64>, std::vec::Vec<f64>)> = std::vec::Vec::with_capacity(labels.len());
+
+    for (item_index, label) in labels.iter().enumerate() {
+        let image_start_index = item_index * pixels_per_image;
+        let image_end_index = (item_index + 1) * pixels_per_image;
+
+        let mut image_vec = std::vec::Vec::with_capacity(pixels_per_image);
+        for pixel in &image_bytes[image_start_index..image_end_index] {
+            image_vec.push(*pixel as f64);
+        }
+        examples.push(( image_vec, vec![*label as f64]));
+    }
+
+    let mut neural_network = nn::NN::new(&[pixels_per_image as u32, 10, 1]); // TODO: Optimize neural network parameters here
+
+    std::println!("Begin training");
+    neural_network.train(&examples).go();
+    std::println!("Training finished");
 }
 
-fn open_labels(path: &str) {
+fn open_labels(path: &str) -> Vec<u8> {
     let mut f = std::fs::File::open(path).unwrap();
 
     let magic_number = read_u32(&mut f);
-    assert!(magic_number == 2049);
+    assert_eq!(magic_number, 2049);
 
     let number_of_items = read_u32(&mut f);
     assert!(number_of_items == 10000 || number_of_items == 60000);
 
-    std::println!("Labels:");
-    for _ in 0..10 {
-        let label = read_byte(&mut f);
-        assert!(label <= 9);
-        std::println!("{}", label)
-    }
+    let mut bytes = Vec::new();
+    f.read_to_end(&mut bytes).unwrap();
+
+    return bytes;
 }
 
-fn open_images(path: &str) {
+fn open_images(path: &str) -> Vec<u8> {
     let mut f = std::fs::File::open(path).unwrap();
 
     let magic_number = read_u32(&mut f);
-    assert!(magic_number == 2051);
+    assert_eq!(magic_number, 2051);
 
     let number_of_items = read_u32(&mut f);
     assert!(number_of_items == 10000 || number_of_items == 60000);
 
     let number_of_rows = read_u32(&mut f);
-    assert!(number_of_rows == 28);
+    assert_eq!(number_of_rows, 28);
 
     let number_of_columns = read_u32(&mut f);
-    assert!(number_of_columns == 28);
+    assert_eq!(number_of_columns, 28);
 
-    let mut images = Vec::new();
-    f.read_to_end(&mut images).unwrap();
+    let mut bytes = Vec::new();
+    f.read_to_end(&mut bytes).unwrap();
 
-    std::println!("Image:");
-    for row in 0..number_of_rows {
-        for column in 0..number_of_columns {
-            let index = (row * number_of_columns + column) as usize;
-            std::print!("{:3} ", images[index]);
-        }
-        std::print!("\n");
-    }
+    return bytes
 }
 
 fn read_u32(file: &mut std::fs::File) -> u32 {
@@ -60,9 +74,13 @@ fn read_u32(file: &mut std::fs::File) -> u32 {
     return u32::from_be_bytes(buffer);
 }
 
-fn read_byte(file: &mut std::fs::File) -> u8 {
-    let mut buffer = [0];
-    let num_bytes_read = file.read(&mut buffer).unwrap();
-    assert!(num_bytes_read == 1);
-    return buffer[0];
+fn print_image(buffer: &Vec<f64>, number_of_rows: u32, number_of_columns: u32) {
+    std::println!("Image:");
+    for row in 0..number_of_rows {
+        for column in 0..number_of_columns {
+            let index = (row * number_of_columns + column) as usize;
+            std::print!("{:3} ", buffer[index]);
+        }
+        std::print!("\n");
+    }
 }
